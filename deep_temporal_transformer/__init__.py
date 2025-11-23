ca35011cfd3a3ce018a51c959e1510bfbc892bc6
Prime-21 <96974921+Prime-21@users.noreply.github.com>
2025-11-23 20:22:02 +0530

    Fixed FocalLoss import - should be FocalLossEnhanced

--- deep_temporal_transformer/__init__.py
+++ deep_temporal_transformer/__init__.py
@@ -7,7 +7,7 @@ __version__ = "2.0.0"
 
 from deep_temporal_transformer.configs.config import Config, get_default_config
 from deep_temporal_transformer.models.advanced_transformer import DeepTemporalTransformerAdvanced
-from deep_temporal_transformer.models.model_enhanced import DeepTemporalTransformerEnhanced, FocalLoss
+from deep_temporal_transformer.models.model_enhanced import DeepTemporalTransformerEnhanced, FocalLossEnhanced
 from deep_temporal_transformer.data.data import DataProcessor
 from deep_temporal_transformer.training.train import ModelTrainer
 from deep_temporal_transformer.models.baseline_enhanced import EnhancedBaselineModels, LSTMBaseline, TemporalCNN
@@ -15,7 +15,7 @@ from deep_temporal_transformer.utils.utils import set_random_seeds, get_device
 
 __all__ = [
     'Config', 'get_default_config', 
-    'DeepTemporalTransformerAdvanced', 'DeepTemporalTransformerEnhanced', 'FocalLoss',
+    'DeepTemporalTransformerAdvanced', 'DeepTemporalTransformerEnhanced', 'FocalLossEnhanced',
     'DataProcessor', 'ModelTrainer', 
     'EnhancedBaselineModels', 'LSTMBaseline', 'TemporalCNN',
     'set_random_seeds', 'get_device'
--- deep_temporal_transformer/models/__init__.py
+++ deep_temporal_transformer/models/__init__.py
@@ -1,13 +1,13 @@
 """Models module - Advanced and enhanced transformer models."""
 
 from deep_temporal_transformer.models.advanced_transformer import DeepTemporalTransformerAdvanced
-from deep_temporal_transformer.models.model_enhanced import DeepTemporalTransformerEnhanced, FocalLoss
+from deep_temporal_transformer.models.model_enhanced import DeepTemporalTransformerEnhanced, FocalLossEnhanced
 from deep_temporal_transformer.models.baseline_enhanced import EnhancedBaselineModels, LSTMBaseline, TemporalCNN
 
 __all__ = [
     'DeepTemporalTransformerAdvanced',
     'DeepTemporalTransformerEnhanced',
-    'FocalLoss',
+    'FocalLossEnhanced',
     'EnhancedBaselineModels',
     'LSTMBaseline',
     'TemporalCNN'
